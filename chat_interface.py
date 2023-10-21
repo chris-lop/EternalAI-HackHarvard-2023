c35bf73f56852196556247b5a780dc32ea8d22dd
Chris Lopez <73512428+chris-lop@users.noreply.github.com>
2023-10-21 18:23:18 -0400

    Form Implementation

--- chat_interface.py
+++ chat_interface.py
@@ -10,6 +10,16 @@ load_dotenv()
 # Setting up API keys from the .env file
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
+def on_button_click(*args):
+    # Fetch data from input fields
+    fav_color = question1.get()
+    watch_frequency = question2.get()
+    device = question3.get()
+    uploaded_image = image_upload.get()
+    
+    # Your logic will go here
+    pass
+
 with gr.Blocks() as demo:
     with gr.Row():
         with gr.Column():
@@ -19,6 +29,24 @@ with gr.Blocks() as demo:
                          '</video>' \
                          '</div>'
             gr.HTML(video_html)
+            
+            # Add questions for user input
+            question1 = gr.Textbox(label="What is your favorite color?")
+            question2 = gr.Textbox(label="How often do you watch videos online?")
+            question3 = gr.Textbox(label="Which device do you primarily use for watching videos?")
+            
+            # Add an Image upload option with a smaller size
+            image_upload = gr.File(
+                label="Upload an image:", 
+                height=70,  # Adjusting the height
+                scale=2,  # Adjusting the relative width
+                min_width=200,  # Setting a minimum width
+                elem_id="custom_upload",  # Setting an id for further CSS customization
+            )
+            
+            # Adding the "Start Chatting" button with an event listener
+            start_chatting_button = gr.Button("Start Chatting", on_click=on_button_click)
+
         with gr.Column():
             chatbot = gr.Chatbot()
             user_input = gr.Textbox(label="Enter your message:")  # Changed the label
